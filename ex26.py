e06801be4622c57e4b53443fdd87c00528187d6e
winttheingihlaing <winttheingihlaing@gmail.com>
2018-06-12 11:44:43 +0630

    My first python ex26

--- ex26.py
+++ ex26.py
@@ -1,6 +1,9 @@
+from sys import argv
+
 print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
+height = input()
 print("How much do you weigh?", end=' ')
 weight = input()
 
@@ -11,56 +14,55 @@ script, filename = argv
 txt = open(filename)
 
 print("Here's your file {filename}:")
-print(tx.read())
+print(txt.read())
 
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
-print(txt_again_read())
+print(txt_again.read())
+
 
 print("Let's practice everything.")
-print('You\'d need to know \'bout escapes 
-        with \\ that do \n newlines and \t tabs.')
+print("You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.")
 
 poem = """
 \tThe lovely world
 with logic so firmly planted
-cannot discern \n the needs oflove 
-nor comprehend passion from intuition 
-and requires an explanation 
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
 \n\t\twhere there is none.
 """
 
-
-print("-----------)
+print("--------------")
 print(poem)
-print(-----------")
+print("--------------")
 
 
-five = 10 - 2 + 3 - 6
-print(f"This should be five: {five}"
+five = 10 - 2 + 3 - 8
+print(f"This should be five: {five}")
 
-def secret_formul(started)
+def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars + 100
+    crates = jars / 100
     return jelly_beans, jars, crates
 
 
 start_point = 10000
-beans, jars = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point)
 
 # remember that this is another way to format a string
 print("With a starting point of: {}".format(start_point))
-# it is just like with an f"" string
+# it's just like with an f"" string
 print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
@@ -72,7 +74,7 @@ dogs = 15
 
 
 if people < cats:
-    print("Too many cats! The world is doomed!")
+    print ("Too many cats! The world is doomed!")
 
 if people < cats:
     print("Not many cats! The world is saved!")
@@ -80,17 +82,18 @@ if people < cats:
 if people < dogs:
     print("The world is drooled on!")
 
-if people > dogs:
-    print("The world is dry!.")
+if people > dogs :
+    print("The world is dry!")
 
 
 dogs += 5
 
-if people >= dogs
+if people >= dogs:
     print("People are greater than or equal to dogs.")
 
-if people <= dogs
+if people <= dogs:
     print("People are less than or equal to dogs.")
 
-if people = dogs:
+
+if people == dogs:
     print("People are dogs.")
