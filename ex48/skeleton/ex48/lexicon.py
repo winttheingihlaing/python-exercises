8103438066d1104671e3647479c2a3e23efa8fd4
winttheingihlaing <winttheingihlaing@gmail.com>
2018-06-17 10:16:54 +0630

    done

--- ex48/skeleton/ex48/lexicon.py
+++ ex48/skeleton/ex48/lexicon.py
@@ -1,12 +1,38 @@
-class Room(object):
+lexicon = {}
+for directions in ['north', 'south', 'east', 'west', 'down', 
+                   'up', 'left', 'right', 'back']:
+    lexicon.update({directions: 'direction'})
+for verbs in ['go', 'stop', 'kill', 'eat']:
+    lexicon.update({verbs: 'verb'})
+for stops in ['the', 'in', 'of', 'from', 'at', 'it']:
+    lexicon.update({stops: 'stop'})
+for nouns in ['door', 'bear', 'princess', 'cabinet']:
+    lexicon.update({nouns: 'noun'})
 
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        self.paths = []
 
-    def go(self, direction):
-        return self.paths.get(direction, None)
 
-    def add_paths(self, paths):
-        self.paths.update(paths)
+stuff = input('> ')
+words = stuff.lower().split()
+
+def scan(sentence):
+    words = sentence.split()
+    pairs = []
+    for word in words:
+        try:
+            k = word.lower()
+            pairs.append((lexicon[k], word))
+        except KeyError:
+            if convert_number(word) != None:
+                 pairs.append(('number', int(word)))
+            else:
+                pairs.append(('error', word))
+    return pairs
+
+def convert_number(s):
+    try:
+        return int(s)
+    except ValueError:
+        return None
+
+
+
