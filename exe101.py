74060bf55019cd0888b14cce04cfc1f734621c65
winttheingihlaing <winttheingihlaing@gmail.com>
2018-06-26 14:27:38 +0630

    python exe101

---
+++ exe101.py
@@ -0,0 +1,15 @@
+tabby_dog = "\tI'm tabbed in."
+persian_dog = "I'm split\non a line."
+backslash_dog = "I'm \\ a \\ cat."
+
+fat_dog = """
+I'll do a list:
+\t* Dog food
+\t* Fishies
+\t* Catnipp\n\t* Grass
+"""
+
+print(tabby_dog)
+print(persian_dog)
+print(backslash_dog)
+print(fat_dog)
